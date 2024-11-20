3d28ab47d47fdf695e1e0f630586c7b5b8471b1f
Ananmay 125 <ananmay125@gmail.com>
2024-11-20 23:09:46 +0530

    img

--- split.co/src/app/page.tsx
+++ split.co/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="justadiv">
-      <img 
+      <Image 
         className="justanimage"
         src="favicon.ico"
         alt="justanimage"
