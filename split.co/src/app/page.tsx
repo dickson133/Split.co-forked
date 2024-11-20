05d7b186df31dc974782172918b4f026923c76c8
Ananmay 125 <ananmay125@gmail.com>
2024-11-20 21:28:12 +0530

    First commit
    W

---
+++ split.co/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="justadiv">
+      <img 
+        className="justanimage"
+        src="favicon.ico"
+        alt="justanimage"
+      />
+    </div>
+  );
+}
